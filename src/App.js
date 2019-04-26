bf6083bde5269566c74bf2dd63db0ef92d24aa9d
Mousa Hakim <shadabadel16@gmail.com>
2019-04-26 16:16:58 +0430

    add like button toggle

--- src/App.js
+++ src/App.js
@@ -2,23 +2,86 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Label extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      status: this.props.text === 'Unlike' ? 'on' : 'off',
+      color: ''
+      
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount(){
+    console.log('Label componentDidMount');
+  }
+
+
+  componentDidUpdate(){
+    console.log('Label componentDidUpdate');
+  }
+
+  handleClick() {
+    this.setState( state => {
+      let status = state.status === 'on' ? 'off' : 'on';
+
+      return {status:status};
+    })
+  }
+
+  render(){
+
+    return <span> {this.props.text}
+            <span onClick={this.handleClick}> Myown state is {this.state.status} </span>
+          </span>;
+
+  }
+}
+
+
+class Button extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+        liked: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+
+    this.setState(state => ({liked:!state.liked}), function(){
+
+    });
+
+    console.log(this.state.liked); 
+
+    
+
+  }
+
+  render(){
+      let text = this.state.liked ? 'Unlike' : 'Like';
+      if (this.state.liked)
+      return <button onClick={this.handleClick}> <Label text={text}/> </button>;
+      else
+        return <button onClick={this.handleClick}> <Label text={text}/> </button>;
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Button/>
     </div>
   );
 }
